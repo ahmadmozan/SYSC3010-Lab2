9b8278e52eba99212648251e7df7dfae317b622f
ahmadmozan <55393935+ahmadmozan@users.noreply.github.com>
2019-09-20 18:24:03 -0400

    Update UDPSender.java

--- UDPSender.java
+++ UDPSender.java
@@ -6,7 +6,7 @@ public class UDPSender {
 	public static void main(String[] args) 
    {
 	      // Check the arguments
-	      if( args.length != 2 )
+	      if( args.length != 3 )
 	      {
 	         System.out.println( "usage: java UDPSender host port" ) ;
 	         return ;
@@ -17,19 +17,27 @@ public class UDPSender {
 	         // Convert the arguments first, to ensure that they are valid
 	         InetAddress host = InetAddress.getByName( args[0] ) ;
 	         int port         = Integer.parseInt( args[1] ) ;
+	         int numMessages = Integer.parseInt( args[2] ) ;
 	         socket = new DatagramSocket() ;
      
 	         Scanner in;
 	         in = new Scanner (System.in);
 	         String message = null;
-	         while (true)
+	         count = 1;
+	         
+	         while (count <= numMessages)
 	         {
-	        		 System.out.println("Enter text to be sent, ENTER to quit ");
+	        		 System.out.println("Message " + count);
 	        		 message = in.nextLine();
 	        		 if (message.length()==0) break;
 	        		 byte [] data = message.getBytes() ;
 	        		 DatagramPacket packet = new DatagramPacket( data, data.length, host, port ) ;
 	        		 socket.send( packet ) ;
+	        		 count++;
+	        		 
+	        		 DatagramPacket packet2 = new DatagramPacket( new byte[PACKETSIZE], PACKETSIZE ) ;
+	 	             socket.receive( packet2 ) ;
+	 	             System.out.println( packet2.getAddress() + " " + packet2.getPort() + ": " + new String(packet2.getData()).trim() ) ;
 	         } 
 	         System.out.println ("Closing down");
 	      }
@@ -44,4 +52,3 @@ public class UDPSender {
       }
    }
 }
-
